e980841b88cb0fab6e048875bbf51179ff5a53c6
ggmanyar@naver.com <ggmanyar@naver.com>
2023-12-26 21:54:10 +0900

    주택 목록 조회 houseType 코드로 변경 -김웅태

--- src/main/java/com/xmonster/howtaxing/dto/house/HouseListResponse.java
+++ src/main/java/com/xmonster/howtaxing/dto/house/HouseListResponse.java
@@ -13,9 +13,9 @@ public class HouseListResponse {
         data = new HashMap<String, Object>();
         List<HouseInfoResponse> list = new ArrayList<HouseInfoResponse>();
 
-        HouseInfoResponse response1 = new HouseInfoResponse("27", "382", "아파트", "반포래미안원베일리아파트", "118동 1403호");
-        HouseInfoResponse response2 = new HouseInfoResponse("88", "382", "아파트", "인덕원퍼스비엘아파트", "108동 1201호");
-        HouseInfoResponse response3 = new HouseInfoResponse("25", "382", "아파트", "반포센트럴자이아파트", "105동 1701호");
+        HouseInfoResponse response1 = new HouseInfoResponse("1", "1", "1", "반포래미안원베일리아파트", "118동 1403호");
+        HouseInfoResponse response2 = new HouseInfoResponse("2", "1", "2", "당동다가구주택", "당동 878-3");
+        HouseInfoResponse response3 = new HouseInfoResponse("3", "1", "4", "대경빌라", "101호");
 
         list.add(response1);
         list.add(response2);
